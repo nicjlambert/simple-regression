4ae3ce8db6108e8dac69f9e18fe5d8b7e36aa652
nicjlambert <lambni85@outlook.com>
2022-07-28 22:10:10 +1000

    added eval parse_expr for filter

--- markov-chain.R
+++ markov-chain.R
@@ -12,46 +12,35 @@ head(weather_data, 10)
 #If today is sunny, what is the probability that tomorrow will be sunny as well? 
 #We simply just need to calculate how many times that today is sunny and the next day is sunny as well from the data.
 
-embed(weather_data, 2)[, 2:1] %>% 
-   as.data.frame() %>% 
-   rename(current = V1, next_day = V2) %>% 
-   filter(current == "sunny") %>% 
-   arrange(next_day) %>% 
-   count(current, next_day)
-
 # Probability if today is cloudy
-
-weather_type <- function(data, weather) {
-embed(data, 2)[, 2:1] %>% 
+prob_weather <- function(df, filter) {
+embed(df, 2)[, 2:1] %>% 
    as.data.frame() %>% 
    rename(current = V1, next_day = V2) %>% 
-   filter(current == weather) %>% 
+   filter(eval(rlang::parse_expr(filter))) %>%
    pull(next_day) %>% 
-   table(paste0("Probability of each weather tomorrow if today is ", cloudy) %>% prop.table()
+   table("Probability of each weather tomorrow if today is cloudy" = .) %>% prop.table()
 }
 
-weather_cloudy <- weather_type(weather_data, "cloudy")
-weather_cloudy
+# Probability if today is cloudy
+weather_cloudy <- prob_weather(weather_data, "current %in% c('cloudy')")
 
 # Probability if today is rainy
-weather_rain <- weather_type(weather_data, "rain")
-weather_rain
+weather_rain <- prob_weather(weather_data, "current %in% c('rain')")
 
 # Probability if today is sunny
-weather_rain <- weather_type(weather_data, "sunny")
-weather_rain
+weather_sunny <- prob_weather(weather_data, "current %in% c('sunny')")
 
+# transition probability matrix
 trans_matrix <- rbind(weather_cloudy, weather_rain, weather_sunny) %>% 
    `rownames<-`(c("cloudy", "rain", "sunny"))
 
 trans_matrix
 
-
 # convert the matrix as a markov chain object
 markov_model <- new("markovchain", 
                     transitionMatrix = trans_matrix, # Input Transition Matrix
                     name = "Weather") # Name of the Markov Chains
-
 markov_model
 set.seed(1)
 plot(markov_model)
